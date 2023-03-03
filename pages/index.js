b85e1b18a1be706efc73134572fc4f86cfba6359
Seiya Terada <seiyaterada123@gmail.com>
2023-03-03 13:41:57 -0800

    implement MUI components

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Index() {
+  return (
+    <Container>
+      <Typography variant="h2" component="h2" align="center">
+        Main Page
+      </Typography>
+    </Container>
+  );
+}
