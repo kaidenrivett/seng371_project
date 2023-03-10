11e120c9ee0aadae7d7168cc5ecaa14e609ff26f
cliu54 <chenghaoliu0229@gmail.com>
2023-03-09 20:50:42 -0800

    working on making user data

---
+++ database/user_data_generator.py
@@ -0,0 +1,38 @@
+import pip
+import names
+import csv
+import random
+
+# Generate names 
+for i in range(100):
+    print(names.get_full_name())
+# Define the list of possible medical conditions
+conditions = ['Diabetes', 'High Blood Pressure', 'Asthma', 'Arthritis', 'Cancer', 'Depression']
+
+# Define the list of possible medications
+medications = ['Metformin', 'Lisinopril', 'Albuterol', 'Ibuprofen', 'Tamoxifen', 'Sertraline']
+department = ['medicine', 'surgery', 'gynaecology',
+              'obstetrics','paediatrics','eye','ENT',
+              'dental', 'orthopaedics', 'neurology', 'cardiology',
+              'psychiatry', 'skin']
+# Generate health records for 100 patients
+health_records = []
+for i in range(1, 101):
+    record = {}
+    record['Patient ID'] = i
+    record['Age'] = random.randint(18, 90)
+    record['Gender'] = random.choice(['Male', 'Female'])
+    record['Medical Condition'] = random.choice(conditions)
+    record['Medication'] = random.choice(medications)
+    record['Dosage'] = random.randint(1, 4)
+    #medicine, surgery, gynaecology, obstetrics, 
+    #paediatrics, eye, ENT, dental, orthopaedics, neurology, cardiology, psychiatry, skin
+    record['Department'] = random.choice(department)
+    health_records.append(record)
+
+# Write the health records to a CSV file
+with open('health_records.csv', mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['Patient ID', 'Age', 'Gender', 'Medical Condition', 'Medication', 'Dosage', 'Frequency'])
+    writer.writeheader()
+    for record in health_records:
+        writer.writerow(record)
