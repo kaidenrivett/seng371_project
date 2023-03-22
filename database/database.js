e71a3ebcbe180a1e8c809e983ff3f279e09a250d
cliu54 <chenghaoliu0229@gmail.com>
2023-03-22 02:58:16 -0700

    code refactoring for patient data generator

--- database/database.js
+++ database/database.js
@@ -1,27 +1,27 @@
-const MongoClient = require('mongodb').MongoClient;
-const csv = require('csv-parser');
-const fs = require('fs');
+// const MongoClient = require('mongodb').MongoClient;
+// const csv = require('csv-parser');
+// const fs = require('fs');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'health_record_db';
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'health_record_db';
 
-MongoClient.connect(url, function(err, client) {
-  if (err) throw err;
+// MongoClient.connect(url, function(err, client) {
+//   if (err) throw err;
 
-  const db = client.db(dbName);
+//   const db = client.db(dbName);
 
-  const results = [];
+//   const results = [];
 
-  fs.createReadStream('./test_data/health_records.csv')
-    .pipe(csv())
-    .on('data', (data) => {
-      results.push(data);
-    })
-    .on('end', () => {
-      db.collection('test').insertMany(results, function(err, result) {
-        if (err) throw err;
-        console.log(`${result.insertedCount} documents inserted`);
-        client.close();
-      });
-    });
-});
+//   fs.createReadStream('./test_data/health_records.csv')
+//     .pipe(csv())
+//     .on('data', (data) => {
+//       results.push(data);
+//     })
+//     .on('end', () => {
+//       db.collection('test').insertMany(results, function(err, result) {
+//         if (err) throw err;
+//         console.log(`${result.insertedCount} documents inserted`);
+//         client.close();
+//       });
+//     });
+// });
--- database/test_data/patient_data_generator.py
+++ database/test_data/patient_data_generator.py
@@ -3,43 +3,51 @@ import names
 import csv
 import random
 
-first_name = []
-last_name = []
-generate_number = 100
+def name_generator(generate_number):
+    first_name = []
+    last_name = []
+    for i in range(generate_number):
+        first_name.append(names.get_full_name().split()[0])
+        last_name.append(names.get_full_name().split()[1])
+    return first_name,last_name
 
-# Generate names create a 100 user sample data
-for i in range(generate_number):
-    first_name.append(names.get_full_name().split()[0])
-    last_name.append(names.get_full_name().split()[1])
-    # print(names.get_full_name())
-
-# Define the list of possible medical conditions
-conditions = ['Diabetes', 'High Blood Pressure', 'Asthma', 'Arthritis', 'Cancer', 'Depression']
-
-# Define the list of possible medications
-medications = ['Metformin', 'Lisinopril', 'Albuterol', 'Ibuprofen', 'Tamoxifen', 'Sertraline']
-department = ['medicine', 'surgery', 'gynaecology',
-              'obstetrics','paediatrics','eye','ENT',
-              'dental', 'orthopaedics', 'neurology', 'cardiology',
-              'psychiatry', 'skin']
-# Generate health records for 100 patients
-health_records = []
-for i in range(1, generate_number+1):
-    record = {}
-    record['First Name'] = first_name[i-1]
-    record['Last Name'] = last_name[i-1]
-    record['Patient ID'] = i
-    record['Age'] = random.randint(18, 90)
-    record['Gender'] = random.choice(['Male', 'Female'])
-    record['Medical Condition'] = random.choice(conditions)
-    record['Medication'] = random.choice(medications)
-    record['Dosage'] = random.randint(1, 4)
-    record['Department'] = random.choice(department)
-    health_records.append(record)
 
 # Write the health records to a CSV file
-with open('health_records.csv', mode='w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=['First Name','Last Name','Patient ID', 'Age', 'Gender', 'Medical Condition', 'Medication', 'Dosage', 'Department'])
-    writer.writeheader()
-    for record in health_records:
-        writer.writerow(record)
+def fileOutput(health_records,record):
+    with open('health_records.csv', mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['First Name','Last Name','Patient ID', 'Age', 'Gender', 'Medical Condition', 'Medication', 'Dosage', 'Department'])
+        writer.writeheader()
+        for record in health_records:
+            writer.writerow(record)
+    
+def main():
+    generate_number = 100
+    first_name , last_name = name_generator(generate_number)
+
+    # Define the list of possible medical conditions
+    conditions = ['Diabetes', 'High Blood Pressure', 'Asthma', 'Arthritis', 'Cancer', 'Depression']
+
+    # Define the list of possible medications
+    medications = ['Metformin', 'Lisinopril', 'Albuterol', 'Ibuprofen', 'Tamoxifen', 'Sertraline']
+    department = ['medicine', 'surgery', 'gynaecology',
+                'obstetrics','paediatrics','eye','ENT',
+                'dental', 'orthopaedics', 'neurology', 'cardiology',
+                'psychiatry', 'skin']
+    # Generate health records for 100 patients
+    health_records = []
+    for i in range(1, generate_number+1):
+        record = {}
+        record['First Name'] = first_name[i-1]
+        record['Last Name'] = last_name[i-1]
+        record['Patient ID'] = i
+        record['Age'] = random.randint(18, 90)
+        record['Gender'] = random.choice(['Male', 'Female'])
+        record['Medical Condition'] = random.choice(conditions)
+        record['Medication'] = random.choice(medications)
+        record['Dosage'] = random.randint(1, 4)
+        record['Department'] = random.choice(department)
+        health_records.append(record)
+    fileOutput(health_records,record)
+
+if __name__ == "__main__":
+    main()
