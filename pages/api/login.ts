189b29050262f5ad75c659ed51162e46e51e3f59
Kyle Stang <kylestang@users.noreply.github.com>
2023-03-23 16:04:04 -0700

    Added authentication

---
+++ pages/api/login.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { getUser, JWT_KEY } from "@lib/auth";
+import { SignJWT } from 'jose';
+
+type RequestData = {
+    user: string,
+    pass: string
+}
+
+type ResponseData = {
+    location: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    // Wrong method
+    if (req.method != "POST") {
+        res.status(405);
+        return;
+    }
+
+    let userData: RequestData;
+    console.log(req.body);
+    userData = req.body;
+
+    // Fields missing
+    if (!userData.user || !userData.pass) {
+        res.status(400)
+        return;
+    }
+
+    let jwtClaims = getUser(userData.user, userData.pass);
+
+    // Login failed
+    if (jwtClaims == null) {
+        res.status(401);
+        return;
+    }
+
+    // Login succesful
+    let token = await new SignJWT(jwtClaims)
+        .setProtectedHeader({alg: "HS256"})
+        .sign(JWT_KEY);
+    let header = "token=" + token +"; Path=/";
+
+    res.setHeader("Set-Cookie", header);
+    res.json({location: "/"});
+}
---
+++ pages/denied.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function denied() {
+  return (
+    <Container>
+      <Typography variant="h2" component="h2" align="center">
+        Access denied
+      </Typography>
+    </Container>
+  );
+}
